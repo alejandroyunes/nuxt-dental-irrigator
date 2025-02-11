714f5cbfbb49a0286554aa6a931fecef322d3810
alejandroyunes <alejandroyunes@outlook.com>
2025-02-11 00:42:31 -0500

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,11 @@
 import { defineNuxtConfig } from "nuxt/config"
 
 export default defineNuxtConfig({
-// compatibilityDate: "2025-01-22",
+  // compatibilityDate: "2025-01-22",
   devtools: { enabled: true },
+
   css: ["~/assets/main.scss"],
+
   modules: [    
   '@formkit/nuxt',
   '@pinia/nuxt',
@@ -11,41 +13,46 @@ export default defineNuxtConfig({
   '@nuxt/image',
   '@zadigetvoltaire/nuxt-gtm'
 ],
-app: {
-  head: {
-    htmlAttrs: { lang: 'es' },
-    title: 'Best Dental Irrigators for Healthy Teeth & Gums in 2025',
-    titleTemplate: '%s',
-    link: [
-      {
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico'
-      }
-    ],
+
+  app: {
+    head: {
+      htmlAttrs: { lang: 'es' },
+      title: 'Best Dental Irrigators for Healthy Teeth & Gums in 2025',
+      titleTemplate: '%s',
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico'
+        }
+      ],
+    },
+    rootAttrs: {
+      id: 'dental-irrigator',
+    },
   },
-  rootAttrs: {
-    id: 'dental-irrigator',
+
+  image: {
+    format: ['webp', 'png'],
+    screens: {
+      xs: 320,
+      md: 1024,
+      xl: 1280,
+    },
   },
-},
-image: {
-  format: ['webp', 'png'],
-  screens: {
-    xs: 320,
-    md: 1024,
-    xl: 1280,
-  },
-},
-gtm: {
-  id: 'GTM-53GHMHBC',
-  defer: false,
-  compatibility: false,
-  enabled: true,
-  debug: false,
-  loadScript: true,
-  enableRouterSync: true,
-  ignoredViews: ['homepage'],
-  trackOnNextTick: false,
-  devtools: false,
-  },  
+
+  gtm: {
+    id: 'GTM-53GHMHBC',
+    defer: false,
+    compatibility: false,
+    enabled: true,
+    debug: false,
+    loadScript: true,
+    enableRouterSync: true,
+    ignoredViews: ['homepage'],
+    trackOnNextTick: false,
+    devtools: false,
+    },
+
+  compatibilityDate: '2025-02-11',
 })
